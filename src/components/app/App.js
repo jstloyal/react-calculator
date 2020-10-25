d1d62d4bf1c1b9ac2d1e6639d070b84101551a83
jstloyal <jstloyalty@gmail.com>
2020-10-25 04:38:33 +0100

    Add style to App

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ButtonPanel from '../ButtonPanel';
+import Display from '../Display';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.container}>
+      <div class={styles.app}>
+        <h2 class={styles.title}>React Calculator</h2>
+        <ButtonPanel />
+        <Display />
+      </div>
+    </div>
+  );
+}
+
+export default App;
