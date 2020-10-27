d4807a88e122043852bf23f2517a73d188d2a233
jstloyal <jstloyalty@gmail.com>
2020-10-27 14:00:57 +0100

    Implement clickHandler in ButtonPanel

--- src/components/button-panel/ButtonPanel.js
+++ src/components/button-panel/ButtonPanel.js
@@ -2,37 +2,37 @@ import React from "react";
 import Button from "../button/Button";
 import styles from "./ButtonPanel.module.css";
 
-function ButtonPanel() {
+function ButtonPanel({clickHandler}) {
   return (
     <div className={styles.panel}>
       <div className={styles.group}>
-        <Button names="AC" color="#dfdfdf" />
-        <Button names="+/-" color="#dfdfdf" />
-        <Button names="%" color="#dfdfdf" />
-        <Button names="÷" />
+        <Button names="AC" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="+/-" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="%" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="÷" clickHandler={clickHandler} />
       </div>
       <div className={styles.group}>
-        <Button names="7" color="#dfdfdf" />
-        <Button names="8" color="#dfdfdf" />
-        <Button names="9" color="#dfdfdf" />
-        <Button names="x" />
+        <Button names="7" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="8" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="9" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="x" clickHandler={clickHandler} />
       </div>
       <div className={styles.group}>
-        <Button names="4" color="#dfdfdf" />
-        <Button names="5" color="#dfdfdf" />
-        <Button names="6" color="#dfdfdf" />
-        <Button names="-" />
+        <Button names="4" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="5" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="6" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="-" clickHandler={clickHandler} />
       </div>
       <div className={styles.group}>
-        <Button names="1" color="#dfdfdf" />
-        <Button names="2" color="#dfdfdf" />
-        <Button names="3" color="#dfdfdf" />
-        <Button names="+" />
+        <Button names="1" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="2" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="3" color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="+" clickHandler={clickHandler} />
       </div>
       <div className={styles.group}>
-        <Button names="0" color="#dfdfdf" wide={100} />
-        <Button names="." color="#dfdfdf" />
-        <Button names="=" />
+        <Button names="0" color="#dfdfdf" wide={100} clickHandler={clickHandler} />
+        <Button names="." color="#dfdfdf" clickHandler={clickHandler} />
+        <Button names="=" clickHandler={clickHandler} />
       </div>
     </div>
   );
