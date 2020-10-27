d4d870bcf24e3b9a02c68c14c98e033483e87777
jstloyal <jstloyalty@gmail.com>
2020-10-27 14:37:22 +0100

    improve appearance of the button and display

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,18 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-const Button = ({ name, color, wide, clickHandler }) => (
+const Button = ({ name, color, wide, clickHandler, }) => (
   
   <button
     type="button"
     className={wide ? styles.wide : styles.normal}
-    onClick={() => clickHandler(name)}
     style={{ backgroundColor: color }}
+    onClick={() => clickHandler(name)}
   >
     {name}
   </button>
-  
-  
 );
 
 Button.propTypes = {
